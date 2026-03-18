438c700c26494bc1f29543af0fa475dde52771ef
Filip Roszczyk <s34001@pjwstk.edu.pl>
2026-03-18 18:10:09 +0100

    Dodano tablice liczb do klasy Program

--- APBD1/Program.cs
+++ APBD1/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿namespace APBD1
+{
+	public class Program 
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Welcome to the Statistics app");
+			int[] numbers = [6, 7, 6, 7, 6, 7];
+		}
+	
+	}
+}
