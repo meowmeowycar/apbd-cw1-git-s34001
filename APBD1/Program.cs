7d4da131e731b0ef1e4f2349aca02e853a63527c
Filip Roszczyk <s34001@pjwstk.edu.pl>
2026-03-18 18:54:21 +0100

    Dodano sprawdzenie tablicy i komunikat bledu dla pustej tablicy

--- APBD1/Program.cs
+++ APBD1/Program.cs
@@ -7,6 +7,12 @@
 			Console.WriteLine("Welcome to the Statistics app");
 			int[] numbers = [6, 7, 6, 7, 6, 7];
 
+			if (numbers.Length == 0 || numbers == null)
+			{
+				Console.WriteLine("Error: no entry data detected");
+				return;
+			}
+
 			foreach (var number in numbers)
 			{
 				Console.WriteLine(number);
