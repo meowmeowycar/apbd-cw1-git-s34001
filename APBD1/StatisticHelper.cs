62c11b1a99ef419d0d49656238d1065bd4d78cec
Filip Roszczyk <s34001@pjwstk.edu.pl>
2026-03-18 19:28:29 +0100

    Dodano metode CalculateAverage

--- APBD1/StatisticHelper.cs
+++ APBD1/StatisticHelper.cs
@@ -2,5 +2,19 @@
 
 public class StatisticHelper
 {
-    
+    public static double CalculateAverage(int[] values)
+    {
+        if (values.Length == 0 || values == null)
+        {
+            return 0;
+        }
+	
+        double sum = 0;
+        foreach (var value in values)
+        {
+            sum = sum + value;
+        }
+
+        return sum / values.Length;
+    }	    
 }
